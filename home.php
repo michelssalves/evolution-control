ee77c59aec18c67774d177151bc6adabf4c046da
unknown <michelssalves@gmail.com>
2022-08-08 20:41:28 -0300

    2022-08-08

--- home.php
+++ home.php
@@ -4,12 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Document</title>
 </head>
 <body>  
     <form action="">
+            <table>
+            <thead>
+                 <th></th>
+            </thead>   
+            <tbody>
+                <td>sasdasdasdas</td>
+                <td>sasdasdasdas</td>
+                <td>sasdasdasdas</td>
+                <td>sasdasdasdas</td>
+                <td>sasdasdasdas</td>
+                
+            </tbody> 
+        
+
+            </table>
            <textarea name="" id="" cols="30" rows="10">
-            
+
            </textarea> 
         <button>Enviar</button>
     </form>
