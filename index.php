106788a1da60fddc89828898c69bdded7d59057b
Michel <michelssalves@gmail.com>
2022-08-09 17:38:43 -0300

    09-08-2022 16:55

--- index.php
+++ index.php
@@ -73,12 +73,12 @@ include 'assets/controllers/config.php';
                         <div class="formedit">
                             <span id="msg-edit"></span>
                             <form id="editevent" method="POST" enctype="multipart/form-data">
-                                <input type="" name="id" id="id" >
+                                <input type="hidden" name="id" id="id" >
                                 <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Profissional</label>
                                 <div class="col-sm-10">
-                                    <select name="nome_profissional" class="form-control" id="nome_profissional">
-                            		
+                                    <select name="id_profissional" class="form-control" id="color">
+                                        <option value="">Selecione</option>			
                                         <?php
                                             $sql = $pdo->prepare("SELECT * FROM profissionais ORDER BY nome_profissional");
                                             $sql->execute();
@@ -93,7 +93,7 @@ include 'assets/controllers/config.php';
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Paciente</label>
                                 <div class="col-sm-10">
-                                    <select name="nome_paciente" class="form-control" id="color">
+                                    <select name="id_paciente" class="form-control" id="color">
                                         <option value="">Selecione</option>			
                                         <?php
                                             $sql = $pdo->prepare("SELECT * FROM pacientes ORDER BY nome_paciente");
