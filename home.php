f2350ed107b3ad22e9f19a991898c73fa3577be0
Michel <michelssalves@gmail.com>
2022-08-09 09:08:34 -0300

    09-08-2022 08:38

--- home.php
+++ home.php
@@ -5,11 +5,11 @@ include 'assets/controllers/cadastrarComentario.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="assets/js/block.js"></script>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
