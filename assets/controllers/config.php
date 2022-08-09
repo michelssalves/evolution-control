0f4087794027f35126157c6f358d5b16c40f4102
unknown <michelssalves@gmail.com>
2022-08-08 21:22:30 -0300

    2022-08-08

--- assets/controllers/config.php
+++ assets/controllers/config.php
@@ -1,7 +1,8 @@
+
 <?php
-$db_name ='338658684_ambiente_de_te';
+$db_name = 'u338658684_ambiente_de_te';
 $db_host= 'localhost';
-$db_user= '338658684_ambiente_de_te';
+$db_user= 'u338658684_ambiente_de_te';
 $db_pass= 'Cavalo*123';
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
