326dc31e4ab14b996345a0ed3892e4e96ff47cd5
Michel <michelssalves@gmail.com>
2022-08-09 09:19:03 -0300

    09-08-2022 08:38

--- home.php
+++ home.php
@@ -22,8 +22,9 @@ include 'assets/controllers/cadastrarComentario.php';
                 <div class="container-xxl">
                     <table class="table table-striped table-bordered table-hoverable">
                         <thead class="thead-dark">
+                            <tr colspan="2">
                             <th><center><img style="width:150px; height:150px" src="assets/img/4.jpg" alt=""></th>
-                            <th><center>Paciente</th>
+                            </tr>
                             <th><center>REFAELE YAEDA SCABIA ALVES</th>
                         </thead>
                     </table>
