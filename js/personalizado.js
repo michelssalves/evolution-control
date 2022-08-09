0e554ef873fc644f76469d1f90415ef90bfd0a41
Michel <michelssalves@gmail.com>
2022-08-09 15:25:20 -0300

    09-08-2022 08:38

--- js/personalizado.js
+++ js/personalizado.js
@@ -25,10 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
             $('#visualizar #start').val(info.event.start.toLocaleString());
             $('#visualizar #end').text(info.event.end.toLocaleString());
             $('#visualizar #end').val(info.event.end.toLocaleString());
-            $('#visualizar #nome_paciente').text(info.event.end.toLocaleString());
-            $('#visualizar #nome_paciente').val(info.event.end.toLocaleString());
-            $('#visualizar #nome_profissional').text(info.event.end.toLocaleString());
-            $('#visualizar #nome_profissional').val(info.event.end.toLocaleString());
+            $('#visualizar #nome_paciente').text(info.event.nome_paciente.toLocaleString());
+            $('#visualizar #nome_paciente').val(info.event.nome_paciente.toLocaleString());
+            $('#visualizar #nome_profissional').text(info.event.nome_profissional.toLocaleString());
+            $('#visualizar #nome_profissional').val(info.event.nome_profissional.toLocaleString());
             $('#visualizar #color').val(info.event.backgroundColor);
             $('#visualizar').modal('show');
         },
