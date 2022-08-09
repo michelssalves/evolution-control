a5a32c6ef8de1d7cccd88c74f265adef26458282
unknown <michelssalves@gmail.com>
2022-08-08 21:36:12 -0300

    2022-08-08

--- home.php
+++ home.php
@@ -9,7 +9,10 @@ include 'assets/controllers/cadastrarComentario.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
+    
+   
     <title>Document</title>
 </head>
 <body>  
