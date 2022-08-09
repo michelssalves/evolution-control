600e0dc624f3b906b8e6e0d6ba511d15ab27bf23
Michel <michelssalves@gmail.com>
2022-08-09 10:02:06 -0300

    09-08-2022 08:38

--- index.php
+++ index.php
@@ -1,39 +1,61 @@
-<?php
-session_start();
-$msgErroLogin = $_SESSION['msg']; 
-include 'assets/controllers/userDataBaseAcess.php';
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.98.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="diesel-control-1.1/assets/css/signin.css">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
-  </head>
-  <body class="text-center">    
-<main class="form-signin w-100 m-auto">
-<div>
-<?php if($msgErroLogin){echo $msgErroLogin;session_destroy();}?>
-</div>
-  <form method="POST">
-    <h1 class="h3 mb-3 fw-normal">Acesso</h1>
-    <div class="form-floating">
-      <input type="text" class="form-control" name="usuario" placeholder="Usuario">
-      <label for="floatingInput">Usuario</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" name="senha" class="form-control" placeholder="Senha">
-      <label for="floatingPassword">Senha</label>
-    </div>
+<!--**
+ * @author Cesar Szpak - Celke - cesar@celke.com.br
+ * @pagina desenvolvida usando FullCalendar,
+ * o código é aberto e o uso é free,
+ * porém lembre-se de conceder os créditos ao desenvolvedor.
+ *-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8' />
+        <link href='css/core/main.min.css' rel='stylesheet' />
+        <link href='css/daygrid/main.min.css' rel='stylesheet' />
+        <script src='js/core/main.min.js'></script>
+        <script src='js/interaction/main.min.js'></script>
+        <script src='js/daygrid/main.min.js'></script>
+        <script src='js/core/locales/pt-br.js'></script>
+        <script>
 
-    <button class="w-100 btn btn-lg btn-primary" name="acao" value="login" type="submit">Logar</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 01-03-2022</p>
-  </form>
-</main>  
-  </body>
+            document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
+
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    locale: 'pt-br',
+                    plugins: ['interaction', 'dayGrid'],
+                    //defaultDate: '2019-04-12',
+                    editable: true,
+                    eventLimit: true,
+                    events: 'list_eventos.php',
+                    extraParams: function () {
+                        return {
+                            cachebuster: new Date().valueOf()
+                        };
+                    }
+                });
+
+                calendar.render();
+            });
+
+        </script>
+        <style>
+
+            body {
+                margin: 40px 10px;
+                padding: 0;
+                font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+                font-size: 14px;
+            }
+
+            #calendar {
+                max-width: 900px;
+                margin: 0 auto;
+            }
+
+        </style>
+    </head>
+    <body>
+
+        <div id='calendar'></div>
+
+    </body>
 </html>
