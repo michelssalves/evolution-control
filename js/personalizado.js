5e7cf2be2a765e6108982063fd7a4dc36bc44d95
Michel <michelssalves@gmail.com>
2022-08-09 17:41:57 -0300

    09-08-2022 16:55

--- js/personalizado.js
+++ js/personalizado.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
             $('#visualizar #id').text(info.event.id);
             $('#visualizar #id').val(info.event.id);
             $('#visualizar #id_paciente').text(info.event.extendedProps.id_paciente);
-            $('#visualizar #id_paciente').val(info.event.extendedProps.id_paciente);
+            $('#visualizar #id_paciente').val(info.event.id_paciente);
             $('#visualizar #id_profissional').text(info.event.extendedProps.id_profissional);
             $('#visualizar #nome_profissional').text(info.event.extendedProps.nome_profissional);
             $('#visualizar #B').val(info.event.extendedProps.nome_profissional);
