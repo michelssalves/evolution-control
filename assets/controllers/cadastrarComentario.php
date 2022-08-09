eb978a9b1684f8dbad95223c38768de75f9b1638
unknown <michelssalves@gmail.com>
2022-08-08 21:15:33 -0300

    2022-08-08

--- assets/controllers/cadastrarComentario.php
+++ assets/controllers/cadastrarComentario.php
@@ -3,21 +3,14 @@ $acao = $_REQUEST['acao'];
 
 if($acao =='evoluir'){
 
-    $id_funcionario = $_REQUEST['id_funcionario'];
-    $comentario = $_REQUEST['comentario'];
-
-    registrarComentario($id_funcionario, $comentario);
-
-}
-function registrarComentario($id_funcionario, $comentario){
-
     include 'config.php';
 
-    $id_funcionario = $_POST['id_funcionario'];
-    $comentario = $_POST['comentario'];
+    $id_funcionario = $_REQUEST['id_funcionario'];
+    $comentario = $_REQUEST['comentario'];
 
         $sql = $pdo->prepare("INSERT INTO comentarios (id_funcionario, comentario) VALUES (:id_funcionario, :comentario)");
         $sql->bindValue(':id_funcionario', $id_funcionario);
         $sql->bindValue(':comentario', $comentario);
+        $sql->execute();
 
-} 
+}
