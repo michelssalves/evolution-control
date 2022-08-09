2af63610d7daf1d486e2e6c2ca95eaea21252dcc
Michel <michelssalves@gmail.com>
2022-08-09 17:10:53 -0300

    09-08-2022 16:55

--- index.php
+++ index.php
@@ -44,12 +44,9 @@ include 'assets/controllers/config.php';
                     <div class="modal-body">
                         <div class="visevent">
                             <dl class="row">
-                                <dt class="col-sm-3">Título do evento</dt>
+                                <dt class="col-sm-3">Id do evento</dt>
                                 <dd  class="col-sm-9" id="id"></dd>
 
-                                <dt class="col-sm-3">Título do evento</dt>
-                                <dd class="col-sm-9" id="title"></dd>
-                                
                                 <dt class="col-sm-3">Início do evento</dt>
                                 <dd class="col-sm-9" id="start"></dd>
 
