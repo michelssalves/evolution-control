9f4f3552e1392ca2c608d408949f81145765b150
Michel <michelssalves@gmail.com>
2022-08-10 10:38:35 -0300

    2022-08-10 08:37

--- evoluir.php
+++ evoluir.php
@@ -10,9 +10,10 @@ include 'assets/controllers/cadastrarComentario.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="assets/js/block.js"></script>
-    <link rel="stylesheet" href="assets/css/style.css">
+   
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Evolução</title>
 </head>
 <body>
