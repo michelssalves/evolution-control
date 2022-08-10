a0b547f921de87eeae0c9b25fc6aa4b791f58d80
Michel <michelssalves@gmail.com>
2022-08-10 14:23:44 -0300

    2022-08-10 14:01

--- evoluir.php
+++ evoluir.php
@@ -10,6 +10,9 @@ include 'assets/controllers/cadastrarComentario.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="assets/js/block.js"></script> 
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>EvoluÃ§Ã£o</title>
@@ -49,3 +52,68 @@ include 'assets/controllers/cadastrarComentario.php';
 </div>
 </body>
 </html>
+<script>
+$(document).ready( function () 
+	{
+		$('#example').dataTable( 
+		{ 
+			columnDefs: [{targets: -1,className: 'dt-body-right'}],
+			"pageLength": 25,
+			 "order": [[ 2, 'asc' ], [ 1, 'asc' ]],
+			 "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50,100,"All"] ],
+			language: 
+			{
+				"sEmptyTable": "Nenhum registro encontrado",
+				"sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+				"sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+				"sInfoFiltered": "(Filtrados de _MAX_ registros)",
+				"sInfoPostFix": "",
+				"sInfoThousands": ".",
+				"sLengthMenu": "_MENU_ resultados por pagina",
+				"sLoadingRecords": "Carregando...",
+				"sProcessing": "Processando...",
+				"sZeroRecords": "Nenhum registro encontrado",
+				"sSearch": "Pesquisar",
+				"oPaginate": 
+				{
+					"sNext": "Próximo",
+					"sPrevious": "Anterior",
+					"sFirst": "Primeiro",
+					"sLast": "Último"
+				},
+				"oAria":
+				{
+					"sSortAscending": ": Ordenar colunas de forma ascendente",
+					"sSortDescending": ": Ordenar colunas de forma descendente"
+				},
+				"select": 
+				{
+					"rows": 
+					{
+						"_": "Selecionado %d linhas",
+						"0": "Nenhuma linha selecionada",
+						"1": "Selecionado 1 linha"
+					}
+				}
+			 }
+		});
+	} 
+);
+function PopupCenter(url, title, w, h) {  
+    // Fixes dual-screen position                         Most browsers      Firefox  
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;  
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;  
+              
+    width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;  
+    height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;  
+              
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;  
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;  
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);  
+  
+    // Puts focus on the newWindow  
+    if (window.focus) {  
+        newWindow.focus();  
+    }  
+}  
+</script>
