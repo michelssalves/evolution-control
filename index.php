f72ba912ed71f2a4f221c834fee74d6632151033
Michel <michelssalves@gmail.com>
2022-08-10 09:52:30 -0300

    2022-08-10 08:37

--- index.php
+++ index.php
@@ -72,10 +72,10 @@ include 'assets/controllers/config.php';
                             <form id="formAPI" method="get" action="evoluir.php?id_paciente">
                             <button type="submit" class="btn btn-success">Evoluir</button>
                             </div>
-                            <div class="col-sm-1">
+                            <div class="col-sm-2">
                             <button class="btn btn-warning btn-canc-vis">Editar</button>
                             </div>
-                            <div class="col-sm-1">
+                            <div class="col-sm-3">
                             <a href="" id="apagar_evento" class="btn btn-danger">Apagar</a>
                             </div>
                             </div>
