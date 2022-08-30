d9feff5851fe02f85a47b68789a4efc80d3fab94
Michel <michelssalves@gmail.com>
2022-08-30 10:35:02 -0300

    2022-08-30 10:20

--- agenda/index.php
+++ agenda/index.php
@@ -13,29 +13,17 @@
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">T.0 IS - Fer</th>
+      <th scope="col">T.0 A - Mayumi</th>
+      <th scope="col">Psico - Raiane</th>
+      <th scope="col">Fono - Debora</th>
+      <th scope="col">Fono</th>
+      <th scope="col">T.O - Aline</th>
+      <th scope="col">Psico - Raiane</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    
   </tbody>
 </table>
 </body>
